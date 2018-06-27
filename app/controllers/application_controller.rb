51d0a453a2d2c0130e3368352ce65b013a231d38
John Eckert <johnteckert@gmail.com>
2018-06-27 11:37:19 -0400

    figuring out protobuf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
+require 'protobuf'
+require 'google/transit/gtfs-realtime.pb'
+require 'net/http'
+require 'uri'
+
 class ApplicationController < ActionController::API
+
+  def index
+    data = Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=" + Rails.application.secrets.mta_key + "_id=21"))
+    feed = Transit_realtime::FeedMessage.decode(data)
+    for entity in feed.entity do
+      if entity.field?(:trip_update)
+        p entity.trip_update
+      end
+    end
+  end
+
 end
