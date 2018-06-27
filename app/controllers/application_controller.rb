cd7c165f3c589db30f038bb2d64cf2d40ce9b1eb
John Eckert <johnteckert@gmail.com>
2018-06-27 13:18:04 -0400

    successfully rendering trip data as json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,13 +6,15 @@ require 'uri'
 class ApplicationController < ActionController::API
 
   def index
-    data = Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=" + Rails.application.secrets.mta_key + "_id=21"))
+    data = Net::HTTP.get(URI.parse("http://datamine.mta.info/mta_esi.php?key=" + Rails.application.secrets.mta_key + "&feed_id=21"))
     feed = Transit_realtime::FeedMessage.decode(data)
+    trip_data = []
     for entity in feed.entity do
       if entity.field?(:trip_update)
-        p entity.trip_update
+        trip_data << entity.trip_update
       end
     end
+    render json: trip_data, status: 200
   end
 
 end
